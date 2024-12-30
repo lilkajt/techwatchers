29d2594e3e056f30a5a629f6fff37dc873d5ea77
unknown <87129505+lilkajt@users.noreply.github.com>
2024-12-30 14:22:32 +0100

    login frontend/backend completed

--- techwatchers.client/src/app/login/login.component.ts
+++ techwatchers.client/src/app/login/login.component.ts
@@ -9,17 +9,28 @@ import { LoginService } from '../services/login/login.service';
 })
 export class LoginComponent {
   loginData = {username: '', password: ''};
+  message: string = '';
+  isError: boolean = false;
   constructor( private loginService: LoginService) { }
 
   onSubmit(){
     console.log('Form submitted:', this.loginData);
+    
     this.loginService.login(this.loginData).subscribe(
       (response) => {
-        console.log(response); //successfull login
+        this.isError = false;
+        this.message = 'Login successful!';
+        console.log(response);
+        // this.router.navigate(['/dashboard']);
       },
       (error) => {
-        console.log(error); // error
-        //handle login failure
+        console.log(error);
+        this.isError = true;
+        if (error.status === 401) {
+          this.message = 'Invalid username or password!';
+        } else {
+          this.message = 'An unexpected error occurred!';
+        }
       }
     );
   }
