30b82bfe16633a6ebc96030fffc7049ce2dc9347
GuKiooo <dawidg200803@gmail.com>
2025-01-01 17:39:05 +0100

    fix: migrations fixed

--- Techwatchers.Server/Migrations/20250101163710_updateUserPasswordLength.Designer.cs
+++ Techwatchers.Server/Migrations/20250101163710_updateUserPasswordLength.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Techwatchers.Server.Migrations
 {
     [DbContext(typeof(TechwatchersContext))]
-    partial class TechwatchersContextModelSnapshot : ModelSnapshot
+    [Migration("20250101163710_updateUserPasswordLength")]
+    partial class updateUserPasswordLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -99,8 +102,8 @@ namespace Techwatchers.Server.Migrations
 
                     b.Property<string>("username")
                         .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("varchar(50)");
+                        .HasMaxLength(256)
+                        .HasColumnType("varchar(256)");
 
                     b.HasKey("id");
 
---
+++ Techwatchers.Server/Migrations/20250101163710_updateUserPasswordLength.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Techwatchers.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateUserPasswordLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "users",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "users",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)"
+            );
+        }
+    }
+}
