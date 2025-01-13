bfd2bc5b41ac80a426f5c45d10b8b4c1dd601769
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-13 16:28:26 +0100

    update tabeli kategoria

---
+++ Techwatchers.Server/Migrations/20250113151821_AddMoreCategories.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Techwatchers.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoreCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "name",
+                keyValue: "testowaKategoria",
+                column: "name",
+                value: "żywotność"
+            );
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "name" },
+                values: new object[,]
+                {
+                    { "ekrany" },
+                    { "naprawa" },
+                    { "diagnostyka" },
+                    { "prezenty" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "name",
+                keyValue: "żywotność",
+                column: "name",
+                value: "testowaKategoria"
+            );
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "name",
+                keyValues: new object[]
+                {
+                    "ekrany",
+                    "naprawa",
+                    "diagnostyka",
+                    "prezenty"
+                });
+        }
+    }
+}
