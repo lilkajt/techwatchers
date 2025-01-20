28a3c14e80aa556a3001ad96b42dc030f1856542
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-20 10:42:41 +0100

    updated migration

--- Techwatchers.Server/Migrations/20250113151821_AddMoreCategories.cs
+++ Techwatchers.Server/Migrations/20250113151821_AddMoreCategories.cs
@@ -10,18 +10,12 @@ namespace Techwatchers.Server.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.UpdateData(
-                table: "categories",
-                keyColumn: "name",
-                keyValue: "testowaKategoria",
-                column: "name",
-                value: "żywotność"
-            );
             migrationBuilder.InsertData(
                 table: "categories",
                 columns: new[] { "name" },
                 values: new object[,]
                 {
+                    { "konfiguracja" },
                     { "ekrany" },
                     { "naprawa" },
                     { "diagnostyka" },
@@ -32,18 +26,12 @@ namespace Techwatchers.Server.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.UpdateData(
-                table: "categories",
-                keyColumn: "name",
-                keyValue: "żywotność",
-                column: "name",
-                value: "testowaKategoria"
-            );
             migrationBuilder.DeleteData(
                 table: "categories",
                 keyColumn: "name",
                 keyValues: new object[]
                 {
+                    "konfiguracja",
                     "ekrany",
                     "naprawa",
                     "diagnostyka",
